6f6913be729e64ee40c7ac0cf665cd77b1682e0e
michael <michael.gallipo@gmail.com>
2021-06-30 10:55:42 -0400

    Added api call and data display, tweaked css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component} from "react";
 import './App.css';
+// import {Button} from 'react-bootstrap'
 
-// function App() {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { weatherData: {}};
+    this.state = { weatherData: {forecast: [{}, {}, {}]}, display: false};
   }
 
   zipcodeSubmit = e => {
@@ -36,28 +36,32 @@ class App extends Component {
             state: data.location.region,
             forecast: [
               {
-              highTemp: data.forecast.forecastday[0].day.maxtemp_f,
-              lowTemp: data.forecast.forecastday[0].day.mintemp_f,
-              rainChance: data.forecast.forecastday[0].day.daily_chance_of_rain,
-              humidity: data.forecast.forecastday[0].day.avghumidity,
-              icon: data.forecast.forecastday[0].day.condition.icon
+                date: data.forecast.forecastday[0].date,
+                highTemp: data.forecast.forecastday[0].day.maxtemp_f,
+                lowTemp: data.forecast.forecastday[0].day.mintemp_f,
+                rainChance: data.forecast.forecastday[0].day.daily_chance_of_rain,
+                humidity: data.forecast.forecastday[0].day.avghumidity,
+                icon: data.forecast.forecastday[0].day.condition.text
               },
               {
+                date: data.forecast.forecastday[1].date,                
                 highTemp: data.forecast.forecastday[1].day.maxtemp_f,
                 lowTemp: data.forecast.forecastday[1].day.mintemp_f,
                 rainChance: data.forecast.forecastday[1].day.daily_chance_of_rain,
                 humidity: data.forecast.forecastday[1].day.avghumidity,
-                icon: data.forecast.forecastday[1].day.condition.icon
+                icon: data.forecast.forecastday[1].day.condition.text
                 },
                 {
+                  date: data.forecast.forecastday[2].date,                  
                   highTemp: data.forecast.forecastday[2].day.maxtemp_f,
                   lowTemp: data.forecast.forecastday[2].day.mintemp_f,
                   rainChance: data.forecast.forecastday[2].day.daily_chance_of_rain,
                   humidity: data.forecast.forecastday[2].day.avghumidity,
-                  icon: data.forecast.forecastday[2].day.condition.icon
+                  icon: data.forecast.forecastday[2].day.condition.text
                   }  
             ]
-          }
+          },
+          display: true
         })
         console.log(this.state.weatherData);
       })
@@ -83,8 +87,56 @@ class App extends Component {
               // value="35.6895"
             />
           </div>
-          <button className="btn btn-lg btn-primary">Submit</button>
+          <button className="submit">Submit</button>
         </form>
+        <a href='https://youtu.be/dQw4w9WgXcQ' target='_blank' rel="noopener noreferrer">
+          <button className="special">Vacation Special</button>
+        </a>
+
+        <br />
+        <>
+        <div className="container">
+          {this.state.display && (
+          <>
+          <h1>{this.state.weatherData.city}, {this.state.weatherData.state}</h1>
+          <table>
+            <tr>
+              <th>Forecast</th>
+              <th>Date</th>
+              <th>High Temp</th>
+              <th>Low Temp</th>
+              <th>humidity</th>
+              <th>Chance of Rain</th>
+            </tr>
+            <tr>
+              <td>{this.state.weatherData.forecast[0].icon}</td>
+              <td>{this.state.weatherData.forecast[0].date}</td>
+              <td>{this.state.weatherData.forecast[0].highTemp}</td>
+              <td>{this.state.weatherData.forecast[0].lowTemp}</td>
+              <td>{this.state.weatherData.forecast[0].humidity}</td>
+              <td>{this.state.weatherData.forecast[0].rainChance}%</td>
+            </tr>
+            <tr>
+              <td>{this.state.weatherData.forecast[1].icon}</td>
+              <td>{this.state.weatherData.forecast[1].date}</td>
+              <td>{this.state.weatherData.forecast[1].highTemp}</td>
+              <td>{this.state.weatherData.forecast[1].lowTemp}</td>
+              <td>{this.state.weatherData.forecast[1].humidity}</td>
+              <td>{this.state.weatherData.forecast[1].rainChance}%</td>
+            </tr>            
+            <tr>
+              <td>{this.state.weatherData.forecast[2].icon}</td>
+              <td>{this.state.weatherData.forecast[2].date}</td>
+              <td>{this.state.weatherData.forecast[2].highTemp}</td>
+              <td>{this.state.weatherData.forecast[2].lowTemp}</td>
+              <td>{this.state.weatherData.forecast[2].humidity}</td>
+              <td>{this.state.weatherData.forecast[2].rainChance}%</td>
+            </tr> 
+          </table>
+          </>
+          )}
+          </div>
+          </>
       </div>
     );
   }
